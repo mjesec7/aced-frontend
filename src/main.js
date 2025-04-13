10c9d1dbbfd28dd369ca7d34168d214703e5927c
mjesec7 <isakulovashakhzoda@gmail.com>
2025-04-13 10:57:06 +0500

    add global responsive styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import './assets/css/responsive.css'; // ⬅️ Add this line
 
 createApp(App).use(router).mount('#app');
