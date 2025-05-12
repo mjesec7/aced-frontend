d150b3f32eff16304387b4ff710d6a2f8664bd65
mjesec7 <isakulovashakhzoda@gmail.com>
2025-05-12 13:02:47 +0500

    fixed app vue

--- src/App.vue
+++ src/App.vue
@@ -7,36 +7,10 @@
 
 <script>
 import LanguageSwitcher from '@/components/Global/LanguageSwitcher.vue';
-import { translateText } from '@/services/translationService';
 
 export default {
   name: 'App',
-  components: { LanguageSwitcher },
-  watch: {
-    '$route'() {
-      this.applyTranslation();
-    }
-  },
-  mounted() {
-    this.applyTranslation();
-  },
-  methods: {
-    async applyTranslation() {
-      const lang = localStorage.getItem('lang') || 'en';
-      const elements = document.querySelectorAll('[data-translate], h1, h2, h3, h4, h5, h6, p, button, span, label, a, li');
-
-      for (const el of elements) {
-        const text = el.dataset.originalText || el.innerText;
-        el.dataset.originalText = text;
-        try {
-          const translated = await translateText(text, lang);
-          el.innerText = translated;
-        } catch (err) {
-          console.warn('Translation failed for:', text);
-        }
-      }
-    }
-  }
+  components: { LanguageSwitcher }
 };
 </script>
 
